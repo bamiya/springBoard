0b0f8dc2c85faf1badd2956da7bbfcaa0021b4e9
Bamiya <bamiyamy@gmail.com>
2023-03-25 18:23:44 +0900

    게시글 목록 확인

--- src/main/java/com/example/springBoard/board/controller/BoardController.java
+++ src/main/java/com/example/springBoard/board/controller/BoardController.java
@@ -4,10 +4,13 @@ import com.example.springBoard.board.dto.BoardDTO;
 import com.example.springBoard.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor  //final이 붙은 필드의 생성자를 자동 생성 @Autowired 필요없음
@@ -25,5 +28,12 @@ public class BoardController {
     public String save(@ModelAttribute BoardDTO boardDTO){
         boardService.save(boardDTO);
         return "index";
+    }
+    @GetMapping("/")
+    public String findAll(Model model){
+        //DB 에서 전체 게시글 데이터를 가져와서 list.html에 보여준다
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
     }
 }
